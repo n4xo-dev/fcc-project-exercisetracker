cc731465db56b07cd2a13e1bc85d14bd2dfd4a4f
Ignacio Lopezosa Serrano <n4xo@pm.me>
2023-05-01 12:27:34 +0200

    fix

--- api/routes/exercisesRouter.js
+++ api/routes/exercisesRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as exercise from '../services/exercisService.js';
+import * as exercise from '../services/exerciseService.js';
 
 const router = express.Router();
 
